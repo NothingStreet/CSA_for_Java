27146f7d7721434cfc8f5bba67aa2f2f97371a0a
无无街 <130387328+NothingStreet@users.noreply.github.com>
2023-05-20 14:09:45 +0800

    Delete 第三次作业 directory

--- 第三次作业/Work3/H1/Compute.java
+++
@@ -1,5 +0,0 @@
-package Work3.H1;
-
-public interface Compute {
-    public abstract int Computer(int n,int m);
-}
--- 第三次作业/Work3/H1/Mul.java
+++
@@ -1,8 +0,0 @@
-package Work3.H1;
-
-public class Mul implements Compute{
-    @Override
-    public int Computer(int n, int m) {
-        return n*m;
-    }
-}
