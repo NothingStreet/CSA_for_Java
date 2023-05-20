021151f3175f85ac3d08a200e5b696fd418e25f0
无无街 <130387328+NothingStreet@users.noreply.github.com>
2023-05-20 14:03:15 +0800

    Add files via upload

---
+++ 第三次作业/Work3/Work3/H1/Compute.java
@@ -0,0 +1,5 @@
+package Work3.H1;
+
+public interface Compute {
+    public abstract int Computer(int n,int m);
+}
---
+++ 第三次作业/Work3/Work3/H2/UnexpectedScoreException.java
@@ -0,0 +1,11 @@
+package Work3.H2;
+
+public class UnexpectedScoreException extends RuntimeException {
+    public UnexpectedScoreException() {
+        super();
+    }
+
+    public UnexpectedScoreException(String message) {
+        super(message);
+    }
+}
